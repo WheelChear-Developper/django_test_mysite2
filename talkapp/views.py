f917f045eb4be3d4ee9c63b5d7103f96b967c9b2
M.Amatani <amatani@mobile-innovation.jp>
2017-07-05 21:29:27 +0900

    ユーザー情報更新修正

--- talkapp/views.py
+++ talkapp/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 #ユーザー認証
 from django.contrib.auth import authenticate, login, logout
+#サーバーの画像ファイル削除必要用
+import os
 
 from datetime import datetime
 
@@ -64,9 +66,65 @@ def user_store(request):
     return redirect('talkapp:post_index')
 
 def user_edit(request):
-    return render(request, 'talkapp/user_edit.html')
+    user = request.user
+    context = {
+        'user' : user
+    }
+    return render(request, 'talkapp/user_edit.html' , context)
 
 def user_update(request):
+    user = request.user
+
+    #名前が渡されてくれた場合、名前変更
+    if request.POST["name"] != '':
+        user.username = request.POST["name"]
+    #メールアドレスが渡されてくれた場合、メールアドレス変更
+    if request.POST["email"] != '':
+        user.email = request.POST["email"]
+    #パスワードが渡されてくれた場合、パスワード変更
+    if request.POST["password"] != '':
+        user.set_password(request.POST["password"])
+
+    #ユーザー情報保存
+    user.save()
+
+    #プロファイル画像更新
+    if 'file' in request.FILES:
+        #削除ファイル
+        print("\033[94m", "delete_image_file = ", user.profile.image, "\033[0m")
+        #更新前のプロファイル画像を削除
+        old_image_file = UPLOAD_DIR + user.profile.image
+        os.remove(old_image_file)
+
+        # システム時間取得
+        now = datetime.now().strftime("%Y%m%d-%H%M%S")
+        print("\033[94m", "system time = ", now, "\033[0m")
+
+        # ファイル名取得（未設定ではエラー）
+        image_file = request.FILES['file']
+        print("\033[94m", "image_file = ", image_file, "\033[0m")
+
+        # パス設定
+        path = UPLOAD_DIR + str(now) + image_file.name
+        print("\033[94m", "path = ", path, "\033[0m")
+
+        # ファイル書き込み
+        destination = open(path, 'wb+')
+        for chunk in image_file.chunks():
+            destination.write(chunk)
+        destination.close()
+
+        # プロファイルデータ保存（ユーザー番号に紐づく画像ファイル設定）
+        user.profile.image = str(now) + image_file.name
+        user.profile.save()
+
+    #ログイン処理
+    if request.POST["password"] != '':
+        user = authenticate(username=user, password=request.POST["password"])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+
     return redirect('talkapp:post_index')
 
 class Post:

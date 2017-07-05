a8839b1707ff01d7ff2d994b17cefc607fda4e3b
M.Amatani <amatani@mobile-innovation.jp>
2017-07-05 16:14:39 +0900

    保存先イメージファイルパス取得

--- talkapp/views.py
+++ talkapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect
-from talkapp.models import PostMessage
-#from django.contrib.auth.models import User
+from talkapp.models import PostMessage, Profile
+from django.contrib.auth.models import User
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+UPLOAD_DIR = settings.MEDIA_ROOT + "/images/"
 
 def home(request):
     return render(request, 'talkapp/home.html')
@@ -13,10 +19,34 @@ def user_store(request):
     email = request.POST["email"]
     password = request.POST["password"]
 
-    print("username:", username)
+    #入力したユーザー情報
+    print("\033[94m", "username = ", username, "\033[0m")
+    print("\033[94m", "email = ", email, "\033[0m")
+    print("\033[94m", "password = ", password, "\033[0m")
 
-#    user = User.objects.create_user(username, email, password)
-#    user.save()
+    #ユーザー情報登録
+    user = User.objects.create_user(username, email, password)
+    user.save()
+
+    print("\033[94m", "UPLOAD_DIR = ", UPLOAD_DIR, "\033[0m")
+
+    now = time.time()
+#    image_file = request.FILES['file']
+#    path = UPLOAD_DIR + str(now) + image_file.name
+#    destination = open(path, 'wb+')
+#    for chunck in image_file.chunk():
+#        destination.write(chunck)
+#    destination.close()
+
+#    profile = profile()
+#    profile.image = str(now) + image_file.name
+#    profile.user = user
+#    profile.save()
+
+#    user = authenticate(username=username, password=password)
+#    if user is not None:
+#        if user.is_active:
+#            login(request, user)
 
     return redirect('talkapp:post_index')
 

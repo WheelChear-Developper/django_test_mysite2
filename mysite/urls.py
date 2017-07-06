9b3168c2c064958d2aca8e6e45ac5bbff41a41cd
Mobile Innovation, LLC <service-in@mobile-innovation.jp>
2017-07-06 23:24:03 +0900

    ルート変更

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^talkapp/', include('talkapp.urls')),
+    url(r'^', include('talkapp.urls')),
 ]

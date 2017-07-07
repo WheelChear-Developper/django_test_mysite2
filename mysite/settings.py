bff90411b16b764be70a120597543e9689e37d49
M.Amatani <amatani@mobile-innovation.jp>
2017-07-07 19:43:34 +0900

    css 更新

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'talkapp')
 STATIC_URL = '/static/'
 
 #イメージファイル設定
